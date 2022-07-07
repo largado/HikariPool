c1dd7d0c8dd616a94a89ee082e8d160b5eb3a676
largado <alexandrohipolito@gmail.com>
2022-07-07 14:58:58 -0300

    Initial commit

---
+++ src/main/java/avena/spring/data/SpringDataApplication.java
@@ -0,0 +1,16 @@
+package avena.spring.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataApplication.class, args);
+
+
+
+	}
+
+}
